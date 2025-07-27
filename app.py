81bc6fc129f6dc4d7dd00732a2515f534bddaad3
naano0t <2426nabila@gmail.com>
2025-07-28 06:51:49 +0700

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ import io
 
 # ----------------- Styling CSS + Background -----------------
 # Ubah warna berdasarkan mode
-if mode == "Dark Mode":
+if mode = "Dark Mode":
     bg_color = "#000000"
     text_color = "#ffffff"
     shadow_color = "#000000"
